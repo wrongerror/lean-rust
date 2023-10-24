58c9ecca655b4decf53d5ec25ee994d9d073fa88
wrongerror <wangyifei@kubesphere.io>
2023-10-24 21:38:27 +0800

    bugfix
    Signed-off-by: wrongerror <wangyifei@kubesphere.io>

--- kprobetcp/kprobetcp-ebpf/src/main.rs
+++ kprobetcp/kprobetcp-ebpf/src/main.rs
@@ -5,8 +5,8 @@ use aya_bpf::{macros::{kprobe, map}, programs::ProbeContext, maps::HashMap};
 use aya_log_ebpf::info;
 use aya_bpf::helpers::bpf_get_current_pid_tgid;
 
-#[map]
-static CONNECTIONS: HashMap<u32, u32> = HashMap::with_max_entries(10240, 0);
+#[map] // (1)
+static mut CONNECTIONS: HashMap<u32, u32> = HashMap::with_max_entries(10240, 0);
 
 #[kprobe]
 pub fn kprobetcp(ctx: ProbeContext) -> u32 {
@@ -16,11 +16,12 @@ pub fn kprobetcp(ctx: ProbeContext) -> u32 {
     }
 }
 
+// (2)
 fn try_kprobetcp(ctx: ProbeContext) -> Result<u32, u32> {
     let pid = bpf_get_current_pid_tgid() as u32;
-    let count: u32 = unsafe { CONNECTIONS.get(&pid).unwrap_or(&0) } + 1;
+    let count: u32 = unsafe { CONNECTIONS.get(&pid).unwrap_or(0) } + 1;
     CONNECTIONS.insert(&pid, &count, 0).unwrap();
-    // info!(&ctx, "function tcp_connect called");
+    info!(&ctx, "function tcp_connect called");
     Ok(0)
 }
 
