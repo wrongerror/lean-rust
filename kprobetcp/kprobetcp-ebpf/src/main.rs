cd216685f8f56426d56f3e06412b7c3a26288e01
wrongerror <wangyifei@kubesphere.io>
2023-10-24 17:49:44 +0800

    bugfix
    Signed-off-by: wrongerror <wangyifei@kubesphere.io>

--- kprobetcp/kprobetcp-ebpf/src/main.rs
+++ kprobetcp/kprobetcp-ebpf/src/main.rs
@@ -17,9 +17,9 @@ pub fn kprobetcp(ctx: ProbeContext) -> u32 {
 }
 
 fn try_kprobetcp(ctx: ProbeContext) -> Result<u32, u32> {
-    let uid = bpf_get_current_uid_gid() as u32;
-    let count: u32 = unsafe { CONNECTIONS.get(&uid).unwrap_or(&0) } + 1;
-    CONNECTIONS.insert(&uid, &count, 0).unwrap();
+    let pid = bpf_get_current_pid_tgid() as u32;
+    let count: u32 = unsafe { CONNECTIONS.get(&pid).unwrap_or(&0) } + 1;
+    CONNECTIONS.insert(&pid, &count, 0).unwrap();
     info!(&ctx, "function tcp_connect called");
     Ok(0)
 }
