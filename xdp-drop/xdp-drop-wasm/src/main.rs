353a558ae75a1e9460e9aa0141535597bd1b872d
wrongerror <wangyifei@kubesphere.io>
2023-10-26 11:00:37 +0800

    Adjust samples
    Signed-off-by: wrongerror <wangyifei@kubesphere.io>

---
+++ xdp-drop/xdp-drop-wasm/src/main.rs
@@ -0,0 +1,58 @@
+use anyhow::Context;
+use aya::{
+    include_bytes_aligned,
+    maps::HashMap,
+    programs::{Xdp, XdpFlags},
+    Bpf,
+};
+use aya_log::BpfLogger;
+use clap::Parser;
+use log::{warn};
+use std::net::Ipv4Addr;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "eth0")]
+    iface: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
+    env_logger::init();
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    #[cfg(debug_assertions)]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/debug/xdp-drop"
+    ))?;
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Bpf::load(include_bytes_aligned!(
+        "../../target/bpfel-unknown-none/release/xdp-drop"
+    ))?;
+    if let Err(e) = BpfLogger::init(&mut bpf) {
+        // This can happen if you remove all log statements from your eBPF program.
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+    let program: &mut Xdp =
+        bpf.program_mut("xdp_firewall").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&opt.iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+
+    // (1)
+    let mut blocklist: HashMap<_, u32, u32> =
+        HashMap::try_from(bpf.map_mut("BLOCKLIST").unwrap())?;
+
+    // (2)
+    let block_addr: u32 = Ipv4Addr::new(1, 1, 1, 1).try_into()?;
+
+    // (3)
+    blocklist.insert(block_addr, 0, 0)?;
+
+    loop {}
+}
